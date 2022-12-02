cb32cc37d1609b03c173d1c2c11993ac46930e92
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-03 00:35:44 +0600

    Update Main.kt

--- src/main/kotlin/lab_4/Main.kt
+++ src/main/kotlin/lab_4/Main.kt
@@ -17,28 +17,15 @@ fun game(inputStream: InputStream = System.`in`, out: PrintStream = outputConsol
 //    if (Board.size == 5) {
     val startWord = "balda"
     val game = MultiGame(StateBalda(startWord)) // ?????
-//    }
-//    else {
-//        val game = MultiGame(StateXO())
-//    }
-
-//        print("Введиет начальное слово: ")
-//        val reader = BufferedReader(inputStream.reader())
-    // вызвать функцию для начального ввода слова!!! (функция: _____)
+    print(game)
 
     while (!game.gameOver) {
         val reader = BufferedReader(inputStream.reader()).readLine()
         when (val G = Input.parse(reader)){
             is Exit -> print("Exit\n")
-            is Step -> if (checkStep(G.point)) game.step(G.point)
+            is Step -> if (game.step(G)) print("") else print("Error\n")
             is TakeBack -> game.takeBack(G.shift)
         }
-
-        // вызов функции для установки параметров
-        // ввод коодинат, буквы, слова
-        // провекра |^|
-        // новое состояние поля
-        // проверка на выигрыш
         print(game)
     }
 }
