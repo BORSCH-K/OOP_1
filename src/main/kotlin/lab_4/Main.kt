b3bf5a888b79fa0c549e6b461f87e019047e0bb7
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-11 00:05:37 +0600

    no end

--- src/main/kotlin/lab_4/Main.kt
+++ src/main/kotlin/lab_4/Main.kt
@@ -3,22 +3,16 @@ package lab_4
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.PrintStream
-
 val outputConsole: PrintStream = PrintStream(System.out, true, "UTF-8")
-
 fun main() {
     Board.size = 5
     game()
 }
-
 fun game(inputStream: InputStream = System.`in`, out: PrintStream = outputConsole) {
-
     // для балды
-
     val startWord = "balda"
     val game = MultiGame(StateBalda(startWord))
-    out.print(game)
-
+    print(game)
     while (!game.gameOver) {
         val reader = BufferedReader(inputStream.reader()).readLine()
         when (val G = Input.parse(reader)){
@@ -28,17 +22,15 @@ fun game(inputStream: InputStream = System.`in`, out: PrintStream = outputConsol
             }
             is Step -> if (game.step(G)) print("") else out.print("Error\n")
             is TakeBack -> if (game.takeBack(G.shift)) out.print("RETURN:\n") else out.print("Error\n")
-//            else -> {}
         }
-        out.print(game)
+        print(game)
     }
+    if (game.gameOver) out.print("Игра окончена!\n")
+}
 //    out.print("Слова игроков:\n")
 //    while (game.state.words1){
 //    if (game.gameOver)
 //        print(game.state.gameResult)
 //    else
 //    print(game.state.endGame)
-
-
 //    }
-}
