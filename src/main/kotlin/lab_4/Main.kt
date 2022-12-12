168c3a2782e24f4aa9fdd0a150bef23c47f1ee94
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-13 00:34:54 +0600

    not sure

--- src/main/kotlin/lab_4/Main.kt
+++ src/main/kotlin/lab_4/Main.kt
@@ -8,7 +8,48 @@ fun main() {
     Board.size = 5
     game()
 }
+fun game(inputStream: Inpackage lab_4
+
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.PrintStream
+
+var outputConsole: PrintStream = PrintStream(System.out, true, "UTF-8")
+fun main() {
+//    Board.size = 5 // балда
+    game()
+}
+
 fun game(inputStream: InputStream = System.`in`, out: PrintStream = outputConsole) {
+    val reader = BufferedReader(inputStream.reader())
+//    val startWord = "balda"
+//    val game = MultiGame(StateBalda(startWord)) // балда
+    val game = MultiGame(StateXO(turn = '0')) // "крестики-нолики"
+    out.print(game)
+    while (!game.gameOver) {
+        when (val i = Input.parse(reader.readLine())) {
+            is Exit -> {
+                out.print("Exit\n")
+                break
+            }
+
+            is Step -> {
+                if (game.step(i)) print("")
+                else {
+                    out.print("Error\n")
+                    continue
+                }
+            }
+
+            is TakeBack -> {
+                if (game.takeBack(i.shift)) out.print("RETURN:\n")
+                else out.print("Error\n")
+            }
+        }
+        out.print(game)
+    }
+    if (game.gameOver) out.print("Игра окончена!\n")
+}putStream = System.`in`, out: PrintStream = outputConsole) {
     // для балды
     val startWord = "balda"
     val game = MultiGame(StateBalda(startWord))
