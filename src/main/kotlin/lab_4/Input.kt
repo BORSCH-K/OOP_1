bb3b15b337652049a60878020d9fcc3855eba210
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-13 00:34:23 +0600

    not sure

--- src/main/kotlin/lab_4/Input.kt
+++ src/main/kotlin/lab_4/Input.kt
@@ -4,18 +4,19 @@ package lab_4
 // но могут быть изменены
 
 // сделать его закрытым!
-
 // sealed? почему именно он?
+
 sealed interface Input {
     companion object {
         fun parse(string: String): Input {
             val str: List<String> = string.split(" ")
-            // распределение по функциям, по следуюющим действиям
+            // распределение по функциям, по следуюющим признакам
             return when {
-                /*ДОДЕЛАТЬ!*/
-                str.size == 4 -> Step(str)
+                str.size == 4 && Board.size == 5 && str[2].length == 1 -> // для балды
+                    Step(str[0].toInt(), str[1].toInt(),listOf(str[2], str[3]))
+                str.size == 2 && Board.size == 3 -> Step(str[0].toInt(), str[1].toInt()) // для ХО
                 str.size == 2 && str[0] == "-1" -> TakeBack(str[1].toInt())
-                else -> Exit()
+                else -> Exit
             }
         }
     }
