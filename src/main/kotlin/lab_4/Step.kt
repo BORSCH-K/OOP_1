a975f88d5df4ffff11aea02fb8cdca026204769f
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-13 00:37:20 +0600

    not sure

--- src/main/kotlin/lab_4/Step.kt
+++ src/main/kotlin/lab_4/Step.kt
@@ -1,13 +1,9 @@
 package lab_4
 
 // данные о ходе
-class Step(s: List<String>) : Input {
-    val x: Int = s[0].toInt()// сделать через сеттер
-
-    val y: Int = s[1].toInt()
-
+class Step(val x: Int, val y: Int, // координаты
     // буква и образуемое слово
-    val param: List<String> = listOf(s[2], s[3])
-
+    val param: List<String> = listOf()
+) : Input {
     val point: Point = Point(x, y)
 }
