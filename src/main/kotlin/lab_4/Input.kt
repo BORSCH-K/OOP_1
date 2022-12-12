1af39a32b53f086fa851ee516b50cd5dda27310d
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-13 01:56:29 +0600

    final

--- src/main/kotlin/lab_4/Input.kt
+++ src/main/kotlin/lab_4/Input.kt
@@ -1,12 +1,7 @@
 package lab_4
 
-// interface, как предварительный договор, содержит условия, которые будут в классе,
-// но могут быть изменены
-
-// сделать его закрытым!
-// sealed? почему именно он?
-
 sealed interface Input {
+    
     companion object {
         fun parse(string: String): Input {
             val str: List<String> = string.split(" ")
@@ -14,8 +9,8 @@ sealed interface Input {
             return when {
                 str.size == 4 && Board.size == 5 && str[2].length == 1 -> // для балды
                     Step(str[0].toInt(), str[1].toInt(),listOf(str[2], str[3]))
-                str.size == 2 && Board.size == 3 -> Step(str[0].toInt(), str[1].toInt()) // для ХО
                 str.size == 2 && str[0] == "-1" -> TakeBack(str[1].toInt())
+                str.size == 2 && Board.size == 3 -> Step(str[0].toInt(), str[1].toInt()) // для ХО
                 else -> Exit
             }
         }
