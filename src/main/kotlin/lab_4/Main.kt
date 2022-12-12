3f2d6d3a794533b14fb54f4062001793055a955e
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-13 02:00:02 +0600

    final

--- src/main/kotlin/lab_4/Main.kt
+++ src/main/kotlin/lab_4/Main.kt
@@ -3,28 +3,21 @@ package lab_4
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.PrintStream
-val outputConsole: PrintStream = PrintStream(System.out, true, "UTF-8")
-fun main() {
-    Board.size = 5
-    game()
-}
-fun game(inputStream: Inpackage lab_4
-
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.PrintStream
 
 var outputConsole: PrintStream = PrintStream(System.out, true, "UTF-8")
 fun main() {
-//    Board.size = 5 // балда
+    Board.size = 5 // балда
     game()
 }
 
+/*Код для балды, чтобы переключить на "крестики-нолики", требуется
+закомментить балду (// балда) и раскомменчить крестики-нолики (// "крестики-нолики")*/
+
 fun game(inputStream: InputStream = System.`in`, out: PrintStream = outputConsole) {
     val reader = BufferedReader(inputStream.reader())
-//    val startWord = "balda"
-//    val game = MultiGame(StateBalda(startWord)) // балда
-    val game = MultiGame(StateXO(turn = '0')) // "крестики-нолики"
+    val startWord = "balda"                     // балда
+    val game = MultiGame(StateBalda(startWord)) // балда
+//    val game = MultiGame(StateXO(turn = '0')) // "крестики-нолики"
     out.print(game)
     while (!game.gameOver) {
         when (val i = Input.parse(reader.readLine())) {
@@ -49,29 +42,4 @@ fun game(inputStream: InputStream = System.`in`, out: PrintStream = outputConsol
         out.print(game)
     }
     if (game.gameOver) out.print("Игра окончена!\n")
-}putStream = System.`in`, out: PrintStream = outputConsole) {
-    // для балды
-    val startWord = "balda"
-    val game = MultiGame(StateBalda(startWord))
-    print(game)
-    while (!game.gameOver) {
-        val reader = BufferedReader(inputStream.reader()).readLine()
-        when (val G = Input.parse(reader)){
-            is Exit -> {
-                out.print("Exit\n")
-                break
-            }
-            is Step -> if (game.step(G)) print("") else out.print("Error\n")
-            is TakeBack -> if (game.takeBack(G.shift)) out.print("RETURN:\n") else out.print("Error\n")
-        }
-        print(game)
-    }
-    if (game.gameOver) out.print("Игра окончена!\n")
 }
-//    out.print("Слова игроков:\n")
-//    while (game.state.words1){
-//    if (game.gameOver)
-//        print(game.state.gameResult)
-//    else
-//    print(game.state.endGame)
-//    }
