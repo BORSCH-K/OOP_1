d920b458117ed4cecae9eff075061c61c61b1274
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-15 21:27:26 +0600

    comment

--- src/main/kotlin/lab_4/StateXOTemp.kt
+++ src/main/kotlin/lab_4/StateXOTemp.kt
@@ -1,17 +1,21 @@
 package lab_4
 
-class StateXOTemp(
+class StateXOTemp( // наследует StateXO
     board: Board = Board(Array(3) { Array(3) { ' ' } }),
     private var turn: Char = '0'
 ) : StateXO(board) {
 
+    // важно переопределить функции (если таковы имеются), возвращающие класс, так как в следующей итерации программа будет видеть объект родителя, а не наследника
     override fun nextState(step: Step): AbstractState {
         turn = if (turn == 'X') '0' else 'X'
         return StateXOTemp(board.setAndCopy(step.point, turn), turn)
     }
+    
     override fun copyState(): AbstractState {
         return StateXOTemp(Board(board), turn)
     }
+    
+    // тот же toString, только вместо "выиграл" "проиграл"
     override fun toString(): String {
         return if (gameResult != null) {
             if (checkWin() == ' ') "Ничья!\n"
