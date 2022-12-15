5128cdcaade2de73a647c1af9324f07e9bd4e719
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-15 21:23:49 +0600

    comment

--- src/main/kotlin/lab_4/Board.kt
+++ src/main/kotlin/lab_4/Board.kt
@@ -1,18 +1,20 @@
 package lab_4
 
 class Board(val cells: Array<Array<Char>>) {
-    constructor(str: String) : this(Array(size) { i -> stringToArray(str.substring(i * size)) })
+    
+    // из строки делает массив из массовов типа char
+    constructor(str: String) : this(Array(size) { i -> stringToArray(str.substring(i * size)) }) // используется при старте игры
 
-    // конструктор для записи доски при отмене хода
-    constructor(board: Board) : this(board.cells.copy())
+    // из передающегося объекта списывает доску и создает новый
+    constructor(board: Board) : this(board.cells.copy()) // используется в лабе
 
     companion object {
-        var size = 3
-        fun stringToArray(string: String): Array<Char> {
+        var size = 3 // размер поля
+        fun stringToArray(string: String): Array<Char> { // функция для конструктора
             return Array(size) { i -> string[i] }
         }
 
-        fun Array<Array<Char>>.copy(): Array<Array<Char>> {
+        fun Array<Array<Char>>.copy(): Array<Array<Char>> { // функция копирования
             return Array(size) { i -> Array(size) { j -> this[i][j] } }
         }
     }
@@ -20,14 +22,16 @@ class Board(val cells: Array<Array<Char>>) {
     // возвращают символ по координатам
     operator fun get(point: Point): Char = cells[point.y][point.x]
     operator fun get(point: Array<Int>): Char = cells[point[1]][point[0]]
+    
     // возвращает ' ', если ход возможен
     fun getOrNull(point: Point): Char? {
         return if (point.x in 0 until size && point.y in 0 until size && get(point) == ' ') ' '
         else null
     }
+    
     // возвращает переписанную доску
     fun setAndCopy(point: Point, c: Char): Board {
-        return Board(cells.apply { this[point.y][point.x] = c }.copy())
+        return Board(cells.apply { this[point.y][point.x] = c }.copy()) 
     }
 
     // true - заполнен
