846de3b8717956d3be0f5604ec33dd3cfb99a074
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-15 21:20:00 +0600

    comment

--- src/main/kotlin/lab_4/AbstractState.kt
+++ src/main/kotlin/lab_4/AbstractState.kt
@@ -6,20 +6,23 @@ package lab_4
 // тоже набор свойств и методов - то, что общее
 
 abstract class AbstractState(val board: Board) {
+    
+    // результат игры
     abstract val gameResult: String?
-    abstract fun copyState() : AbstractState//????
+    // копирование состояния
+    abstract fun copyState() : AbstractState
 
     // проверка правильности хода и сам ход
-    fun step(step: Step): AbstractState?{
+    fun step(step: Step): AbstractState?{ // в других файлах не встречается, так как метод уже определен и переопределения не трубует
         if (checkStep(step)) {
             return nextState(step)
         }
         return null
     }
 
-    // проверка правильности хода игры + пустое поле или нет?
+    // проверка возможности хода, переопределена в других файлах
     open fun checkStep(step: Step):Boolean{
-        return (step.x in 0  until  Board.size && step.y in 0  until Board.size) // add параметры
+        return (step.x in 0  until  Board.size && step.y in 0  until Board.size) 
     }
 
     // если ход правильный с помощью checkStep, то вызываем nextState и возвращаем новое состояние
