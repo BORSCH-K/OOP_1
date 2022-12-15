febe1fab13913d0246aff9bc6e8f4d0192300f9f
Ксения <102918647+BORSCH-K@users.noreply.github.com>
2022-12-15 22:06:35 +0600

    comment

--- src/main/kotlin/lab_4/MultiGame.kt
+++ src/main/kotlin/lab_4/MultiGame.kt
@@ -1,46 +1,53 @@
 package lab_4
 
+// класс, который отвечает за действия игры, а не за отдельнее ее элементы
+// алгоритм проведения игры не зависит от того, какая это игра
 class MultiGame(state: AbstractState) {
 
-    private val states = ArrayList<AbstractState>()
-    private var indexState = 0
+    private val states = ArrayList<AbstractState>() // история ходов
+    private var indexState = 0 // номер хода
 
     init {
-        if (indexState == 0) states.add(state)
+        if (indexState == 0) states.add(state) // в самом начале игры, записывается пустое поле
     }
 
     private val state: AbstractState
         get() {
-            return states[indexState].copyState()
+            return states[indexState].copyState() // возвращается текущее поле
         }
 
+    // проверяет закончилась ли игра
     val gameOver: Boolean
         get() {
             return state.gameResult != null
         }
 
-    fun step(step0: Step): Boolean {
-        val s = state.step(step0)
+    // ход игры
+    fun step(step0: Step): Boolean { // возвращает true, если ход прошел
+        val s = state.step(step0) // если ход возможен s = состояние, иначе null
         return if (s != null) {
-            states.add(s.copyState())
-            indexState++
+            states.add(s.copyState()) // если ход прошел, то записывает в историю ходов
+            indexState++ 
             true
         } else false
     }
 
-    fun takeBack(shift: Int): Boolean {
+    // отмена хода
+    fun takeBack(shift: Int): Boolean { // если вернулись на "н-ый" ход - true
+        // если отмена возможна
         return if (shift in 0 until indexState) {
             indexState = shift
-            while (states.lastIndex != indexState) {
+            while (states.lastIndex != indexState) { // удаляем все прошлые состояния
                 states.removeAt(states.lastIndex)
             }
             true
         } else false
     }
 
+    // поле
     override fun toString(): String {
         return if (!gameOver)
-            "Ход: $indexState\n" /*+ "Игрок: ${indexState%2+1}\n"*/ + states[indexState]
+            "Ход: $indexState\n" + states[indexState]
         else
             state.toString()
     }
